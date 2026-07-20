1fe1cebd4b2c11f9b87701501a9757d4d824d12b
natankw <hll931200@gmail.com>
2026-07-20 08:07:39 -0300

    Update produto.js

--- js/produto.js
+++ js/produto.js
@@ -2,7 +2,7 @@ const area = document.getElementById("produto");
 
 
 const id = new URLSearchParams(
-window.location.search
+    window.location.search
 ).get("id");
 
 
@@ -11,73 +11,75 @@ const produto = buscarProduto(id);
 
 
 
-
-
 if(!produto){
 
 
-area.innerHTML = `
+    area.innerHTML = `
 
-<div class="card">
+    <div class="card">
 
-<h1>❌ Produto não encontrado</h1>
+    <h1>❌ Produto não encontrado</h1>
 
-</div>
+    </div>
 
-`;
+    `;
 
 
 }else{
 
 
-area.innerHTML = `
+    area.innerHTML = `
 
 
-<div class="card produto">
+    <div class="card produto">
 
 
-<img src="${produto.imagem}" class="thumb">
+        <img 
+        src="${produto.imagem}" 
+        class="thumb"
+        >
 
 
-<h1>${produto.nome}</h1>
+        <h1>
+        ${produto.nome}
+        </h1>
 
 
-<p>
-${produto.descricao}
-</p>
-
-
-<h2>
-💰 R$ ${produto.preco}
-</h2>
-
-
-
-<p>
-📂 ${produto.categoria}
-</p>
+        <p>
+        ${produto.descricao}
+        </p>
 
 
 
-<p>
-📦 Tipo: ${produto.tipo}
-</p>
+        <h2>
+        💰 R$ ${produto.preco}
+        </h2>
 
 
 
-<button onclick="comprar()">
-
-🛒 Comprar agora
-
-</button>
+        <p>
+        📂 Categoria: ${produto.categoria}
+        </p>
 
 
 
-</div>
+        <p>
+        📦 Tipo: ${produto.tipo}
+        </p>
 
 
-`;
 
+        <button onclick="comprar()">
+
+        🛒 Comprar agora
+
+        </button>
+
+
+    </div>
+
+
+    `;
 
 
 }
@@ -85,26 +87,84 @@ ${produto.descricao}
 
 
 
+
+
 function comprar(){
 
 
-/*
-Depois vamos trocar esse link
-pelo checkout Mercado Pago
-*/
+    const dados = {
 
 
-let mensagem = 
-`Olá, quero comprar: ${produto.nome}`;
+        id: produto.id,
+
+
+        nome: produto.nome,
+
+
+        preco: produto.preco
 
 
 
-window.location.href =
+    };
 
-"https://wa.me/" 
-+ produto.whatsapp
-+ "?text="
-+ encodeURIComponent(mensagem);
+
+
+
+    fetch("http://localhost:3000/criar-pagamento",{
+
+
+        method:"POST",
+
+
+        headers:{
+
+
+            "Content-Type":"application/json"
+
+
+        },
+
+
+        body: JSON.stringify(dados)
+
+
+
+    })
+
+
+
+    .then(res=>res.json())
+
+
+    .then(data=>{
+
+
+        console.log(data);
+
+
+
+        alert(
+        "✅ Pedido enviado para pagamento"
+        );
+
+
+    })
+
+
+
+    .catch(error=>{
+
+
+        console.log(error);
+
+
+
+        alert(
+        "❌ Servidor de pagamento offline"
+        );
+
+
+    });
 
 
 
