ce0e3e4cc478d65ba1d72d1a15b7498995a9bbb9
natankw <hll931200@gmail.com>
2026-07-20 08:06:02 -0300

    Update server.js

--- servidor/server.js
+++ servidor/server.js
@@ -1,1 +1,58 @@
+const express = require("express");
+const cors = require("cors");
 
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json());
+
+
+
+// teste do servidor
+
+app.get("/", (req,res)=>{
+
+    res.json({
+        status:"Servidor online"
+    });
+
+});
+
+
+
+// criar pagamento (vamos ativar depois)
+
+app.post("/criar-pagamento",(req,res)=>{
+
+
+    const produto = req.body;
+
+
+
+    res.json({
+
+        mensagem:"Pagamento ainda não configurado",
+
+        produto: produto
+
+    });
+
+
+});
+
+
+
+
+const PORT = 3000;
+
+
+app.listen(PORT,()=>{
+
+    console.log(
+        "Servidor rodando na porta "+PORT
+    );
+
+});
