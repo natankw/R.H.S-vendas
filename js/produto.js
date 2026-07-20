3c5202f1be2016adf026e0cfb2aae7bb50986cd8
natankw <hll931200@gmail.com>
2026-07-20 07:55:09 -0300

    Update produto.js

--- js/produto.js
+++ js/produto.js
@@ -2,7 +2,7 @@ const area = document.getElementById("produto");
 
 
 const id = new URLSearchParams(
-    window.location.search
+window.location.search
 ).get("id");
 
 
@@ -11,57 +11,72 @@ const produto = buscarProduto(id);
 
 
 
+
+
 if(!produto){
 
 
-    area.innerHTML = `
-    
-    <div class="card">
-    
-    <h1>❌ Produto não encontrado</h1>
-    
-    </div>
-    
-    `;
+area.innerHTML = `
+
+<div class="card">
+
+<h1>❌ Produto não encontrado</h1>
+
+</div>
+
+`;
 
 
 }else{
 
 
-    area.innerHTML = `
+area.innerHTML = `
 
 
-    <div class="card">
+<div class="card produto">
 
 
-    <img src="${produto.imagem}" class="thumb">
+<img src="${produto.imagem}" class="thumb">
 
 
-    <h1>${produto.nome}</h1>
+<h1>${produto.nome}</h1>
 
 
-    <p>
-    ${produto.descricao}
-    </p>
+<p>
+${produto.descricao}
+</p>
 
 
-    <h2>
-    💰 R$ ${produto.preco}
-    </h2>
+<h2>
+💰 R$ ${produto.preco}
+</h2>
 
 
 
-    <button onclick="comprar()">
-
-    🛒 Comprar agora
-
-    </button>
+<p>
+📂 ${produto.categoria}
+</p>
 
 
-    </div>
+
+<p>
+📦 Tipo: ${produto.tipo}
+</p>
 
 
-    `;
+
+<button onclick="comprar()">
+
+🛒 Comprar agora
+
+</button>
+
+
+
+</div>
+
+
+`;
 
 
 
@@ -70,16 +85,27 @@ if(!produto){
 
 
 
-
 function comprar(){
 
 
-    // Temporário: vamos trocar pelo checkout real
+/*
+Depois vamos trocar esse link
+pelo checkout Mercado Pago
+*/
 
-    window.location.href =
-    "https://wa.me/"+produto.whatsapp+
-    "?text=Olá, quero comprar o produto: "
-    +produto.nome;
+
+let mensagem = 
+`Olá, quero comprar: ${produto.nome}`;
+
+
+
+window.location.href =
+
+"https://wa.me/" 
++ produto.whatsapp
++ "?text="
++ encodeURIComponent(mensagem);
+
 
 
 }
